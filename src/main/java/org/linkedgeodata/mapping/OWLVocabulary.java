2d1ce36ca1768ccd232abe42c16462277fadef11
jenslehmann1 <jenslehmann1@00633da9-98c1-349d-95eb-89b68c819386>
2010-03-24 10:00:45 +0000

    updated file headers
    git-svn-id: http://linkedgeodata.googlecode.com/svn/trunk@21 00633da9-98c1-349d-95eb-89b68c819386

--- src/main/java/org/linkedgeodata/mapping/OWLVocabulary.java
+++ src/main/java/org/linkedgeodata/mapping/OWLVocabulary.java
@@ -1,7 +1,7 @@
 /**
- * Copyright (C) 2007-2008, Jens Lehmann
+ * Copyright (C) 2009-2010, LinkedGeoData developers
  *
- * This file is part of DL-Learner.
+ * This file is part of LinkedGeoData.
  * 
  * DL-Learner is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
