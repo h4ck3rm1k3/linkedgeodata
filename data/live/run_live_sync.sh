77c75291a5e4ecee6be7150bec784a8e82a2daa3
RavenArkadon <RavenArkadon@00633da9-98c1-349d-95eb-89b68c819386>
2010-07-05 15:29:01 +0000

    Added non-working version of the sync script.
    git-svn-id: http://linkedgeodata.googlecode.com/svn/trunk@91 00633da9-98c1-349d-95eb-89b68c819386

---
+++ data/live/run_live_sync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "This script is not done yet, however it already contains the neccessary statements commented out."
+
+
+echo "Starting an infinite loop of fetching data and patching the database..."
+while [ 1 ]
+do
+# /home/raven/Projects/Current/Eclipse/osmosis/bin/osmosis --rrii workingDirectory=.
+
+# osmosis --rri workingDirectory=. --write-xml-change diff.osc
+
+# osmosis --read-xml-change file="diff.osc" --simplify-change --write-pgsql-change host="localhost" database="unittest_lgd" user="postgres" password="postgres"
+	echo "Testing..."
+	sleep 2
+done
+
