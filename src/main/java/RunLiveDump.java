b4f016cc1417480677a5b2a40628ff56156e4830
RavenArkadon <RavenArkadon@00633da9-98c1-349d-95eb-89b68c819386>
2010-11-11 17:57:43 +0000

    ups, uncommented main method
    git-svn-id: http://linkedgeodata.googlecode.com/svn/trunk@215 00633da9-98c1-349d-95eb-89b68c819386

--- src/main/java/RunLiveDump.java
+++ src/main/java/RunLiveDump.java
@@ -1,8 +1,10 @@
+import org.linkedgeodata.scripts.LiveDump;
+
 
 public class RunLiveDump
 {
 	public static void main(String[] args) throws Throwable
 	{
-		//LiveDump.main(args);
+		LiveDump.main(args);
 	}
 }
